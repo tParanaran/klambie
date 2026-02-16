d40bd868a6e9788e608c779c8eb9e450f91a1f32
salim45d <salim.agus@live.com>
2026-02-17 01:07:22 +0700

    feat: create login form and install package Formik and Yup validation

--- apps/web/src/views/pages/login/index.tsx
+++ apps/web/src/views/pages/login/index.tsx
@@ -1,16 +1,16 @@
-'use client';
 import LoginHero from './components/loginHero';
 import LoginHeader from './components/loginHeader';
+import LoginForm from './components/login-form';
 
 export default function LoginView() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[1.2fr_1fr] lg:grid-cols-2 py-[5%]">
+    <div className="grid grid-cols-1 md:grid-cols-[1.2fr_1fr] lg:grid-cols-2 py-[1%]">
       <LoginHero />
       <div className="py-5 sm:py-10 lg:py-24">
         <div className="hidden md:block">
           <LoginHeader />
         </div>
-        {/* <LoginForm /> */}
+        <LoginForm />
       </div>
     </div>
   );
---
+++ apps/web/src/views/pages/login/schema/login-schema.ts
@@ -0,0 +1,13 @@
+import { object, string } from 'yup';
+
+export const LoginSchema = object({
+  email: string().label('Email').email().min(10).max(100).required(),
+  password: string()
+    .label('Password')
+    .min(8)
+    .required()
+    .matches(
+      /^(?=.*[\d])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+=?/\|"':;><.,`~}{}])(?!.*\s).{6,}$/,
+      'Password must contain a combination of one lowercase, one uppercase, one number, one symbol and no space',
+    ),
+});
