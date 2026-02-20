67596f35fa9c1476d5c88cdf12cd9ae92420f08a
salim45d <salim.agus@live.com>
2026-02-21 00:31:40 +0700

    feat: add login validation

--- apps/api/src/middlewares/validations/auth.validation.ts
+++ apps/api/src/middlewares/validations/auth.validation.ts
@@ -56,3 +56,36 @@ export const EmailValidation = [
     }
   },
 ];
+
+export const LoginValidation = [
+  body('email')
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Invalid email')
+    .isLength({ min: 10, max: 100 })
+    .withMessage('Email must be at least 10 characters'),
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required')
+    .isLength({ min: 8 })
+    .withMessage('Email must be at least 8 characters')
+    .matches(
+      /^(?=.*[\d])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+=?/\|"':;><.,`~}{}])(?!.*\s).{6,}$/,
+    )
+    .withMessage(
+      'Password need to have atleast 1 number and special characters',
+    ),
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const errors = validationResult(req);
+      console.log(errors.array());
+
+      if (!errors.isEmpty()) throw new Error(errors.array()[0].msg);
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+];
