0853d0fab4a1428c79cbdcdc32b50503900907e3
tParanaran <bhaguskara@gmail.com>
2026-03-24 15:28:03 +0700

    feat: change button arrow forward layout

--- apps/web/src/views/pages/d/components/shopByCategory.tsx
+++ apps/web/src/views/pages/d/components/shopByCategory.tsx
@@ -8,7 +8,6 @@ import { IoArrowForward, IoClose } from 'react-icons/io5';
 import { BiCategory } from 'react-icons/bi';
 import TagButton from '@/views/components/tagButton';
 import Image from 'next/image';
-import LinkButton from '@/views/components/link';
 import HorizontalScrollButton from './buttonScroll';
 
 interface IShopByCategory {
@@ -27,7 +26,7 @@ export default function ShopByCategory({ slug }: IShopByCategory) {
   return (
     <main>
       <div className="w-10/12 md:w-8/12 text-center mx-auto">
-        <h1 className="font-bold text-2xl sm:text-4xl lg:text-5xl md:leading-12 my-5">
+        <h1 className="font-bold text-2xl sm:text-4xl lg:text-5xl md:leading-12 my-10">
           Elevate Your Style With Our {category.name} Collections
         </h1>
       </div>
@@ -36,50 +35,54 @@ export default function ShopByCategory({ slug }: IShopByCategory) {
           {segments.map((cat, c) => (
             <div
               key={cat.slug}
-              className={`relative rounded-2xl flex-none cursor-pointer overflow-hidden w-50 h-50 sm:w-auto ${c === 1 || c === 3 ? 'md:row-span-2 md:h-72 lg:h-80 md:mt-auto' : c === 5 || c === 6 ? 'sm:h-32! md:h-28 lg:h-32' : 'md:h-48 lg:h-56'} ${c === 2 ? 'md:row-span-2 md:mt-auto md:h-56 lg:h-64' : ''} ${c === 1 ? 'sm:row-span-2! sm:mt-auto sm:h-80' : ''} ${c === 5 ? 'sm:col-span-2 md:col-span-1' : ''}`}
+              className={`relative flex-none cursor-pointer overflow-hidden w-50 h-50 sm:w-auto ${c === 1 || c === 3 ? 'md:row-span-2 md:h-72 lg:h-80 md:mt-auto' : c === 5 || c === 6 ? 'sm:h-32! md:h-28 lg:h-32' : 'md:h-48 lg:h-56'} ${c === 2 ? 'md:row-span-2 md:mt-auto md:h-56 lg:h-64' : ''} ${c === 1 ? 'sm:row-span-2! sm:mt-auto sm:h-80' : ''} ${c === 5 ? 'sm:col-span-2 md:col-span-1' : ''}`}
             >
-              <Image
-                height={224}
-                width={224}
-                src={
-                  `/images/${category.slug}/${cat.slug}.jpg` ||
-                  '/images/hat.jpg'
-                }
-                alt={cat.name}
-                className="object-cover w-full h-full transition-transform duration-300"
-              />
-
-              <div
-                className={`absolute top-0 w-full bg-linear-to-t dark:bg-linear-to-b from-black/70 to-50% h-full p-2 ${
-                  activeIndex === c ? 'opacity-0' : 'opacity-100'
-                }`}
-              >
-                <h1
-                  className={`${antonFont.className} font-semibold text-lg uppercase text-[#1b1a1e] dark:text-[#ededed]`}
-                >
-                  {cat.name}
-                </h1>
-              </div>
-
-              {activeIndex !== c && (
-                <button
-                  className="rounded-full bg-[#1b1a1e] text-[#ededed] p-1 mx-1 text-2xl absolute bottom-2 right-2 rotate-45"
-                  aria-label="See more"
-                  onClick={
-                    cat.subcategories?.length === 0
-                      ? () => router.push(`/c/${category.slug}/${cat.slug}`)
-                      : () => setActiveIndex(activeIndex === c ? null : c)
+              <div className="absolute inset-0 bg-[#ededed] dark:bg-[#1b1a1e] rounded-2xl -z-10" />
+              <div className="relative w-full h-full rounded-2xl overflow-hidden">
+                <Image
+                  height={224}
+                  width={224}
+                  src={
+                    `/images/${category.slug}/${cat.slug}.jpg` ||
+                    '/images/hat.jpg'
                   }
+                  alt={cat.name}
+                  className="object-cover w-full h-full transition-transform duration-300"
+                />
+
+                <div
+                  className={`absolute top-0 w-full bg-linear-to-t dark:bg-linear-to-b from-black/70 to-50% h-full p-2 ${
+                    activeIndex === c ? 'opacity-0' : 'opacity-100'
+                  }`}
                 >
-                  <IoArrowForward />
-                </button>
-              )}
+                  <h1
+                    className={`${antonFont.className} font-semibold text-lg uppercase text-[#1b1a1e] dark:text-[#ededed]`}
+                  >
+                    {cat.name}
+                  </h1>
+                </div>
+              </div>
+              <div className="absolute z-10 bg-bl bottom-0 right-0 w-9 h-9 bg-[rgb(var(--background-start-rgb))]! rounded-tl-2xl rounded-br-2xl inverted-right"></div>
+
+              <button
+                className={`absolute bottom-0 right-0 z-10 
+                  rounded-full bg-[#1b1a1e] dark:bg-[#ededed] dark:text-[#1b1a1e]  text-white p-1 text-2xl
+                   shadow-lg
+                  transition-all duration-300
+                   ${activeIndex === c ? 'rotate-45' : 'rotate-225'}`}
+                aria-label="See more"
+                onClick={
+                  cat.subcategories?.length === 0
+                    ? () => router.push(`/c/${category.slug}/${cat.slug}`)
+                    : () => setActiveIndex(activeIndex === c ? null : c)
+                }
+              >
+                <IoArrowForward />
+              </button>
 
               {cat.subcategories && cat.subcategories.length > 0 && (
                 <div
-                  className={`absolute inset-0 dark:text-[#ededed] dark:bg-[#1b1a1e]/80 text-black bg-[#ededed]/80 backdrop-blur-xs p-1.5 flex flex-col justify-start transition-all duration-300
-            ${activeIndex === c ? 'opacity-100 translate-y-0 pointer-events-auto' : 'opacity-0 translate-y-5 pointer-events-none'}
-             `}
+                  className={`absolute inset-0 dark:text-[#ededed] dark:bg-[#1b1a1e]/80 text-black rounded-2xl bg-[#ededed]/80 backdrop-blur-xs p-1.5 flex flex-col justify-start transition-all duration-300 ${activeIndex === c ? 'opacity-100 translate-y-0 pointer-events-auto' : 'opacity-0 translate-y-5 pointer-events-none'}`}
                 >
                   <p className="mb-3 opacity-50 text-sm md:text-base">
                     {cat.name}
@@ -106,12 +109,6 @@ export default function ShopByCategory({ slug }: IShopByCategory) {
                       ))}
                     </div>
                   </div>
-                  <button
-                    className="text-2xl absolute top-0 right-0 hover:scale-125 p-2"
-                    onClick={() => setActiveIndex(activeIndex === c ? null : c)}
-                  >
-                    <IoClose />
-                  </button>
                 </div>
               )}
             </div>
