e88033829a6ea767294ab2f0c077e95c65810014
tParanaran <bhaguskara@gmail.com>
2026-03-28 17:18:06 +0700

    feat: fixed color styling

--- apps/web/src/views/pages/login/components/contactLink.tsx
+++ apps/web/src/views/pages/login/components/contactLink.tsx
@@ -5,21 +5,21 @@ const div =
   'absolute flex flex-col-reverse sm:flex-row bottom-2 left-2 right-2  sm:bottom-5 sm:left-5 sm:right-5 gap-2 w-auto z-auto';
 const link =
   'w-full py-1 rounded-full text-light flex justify-between items-center overflow-hidden text-sm sm:flex-row-reverse';
-const span = 'rounded-full text-dark bg-light p-2 mx-1 text-2xl';
+const span = 'rounded-full p-2 mx-1 text-2xl';
 const h1 = 'uppercase mr-5 sm:ml-5';
 
 export default function ContactLink() {
   return (
     <div className={div}>
-      <Link href={'/register'} className={`bg-light text-dark ${link}`}>
-        <span className={span} aria-label="Register">
+      <Link href={'/register'} className={`bg-light text-fixed ${link}`}>
+        <span className={`text-light bg-dark ${span}`} aria-label="Register">
           <IoArrowDown />
         </span>
         <h1 className={h1}>Get Started</h1>
       </Link>
 
       <Link href={'/contact'} className={`border ${link}`}>
-        <span className={span} aria-label="Contact us">
+        <span className={`text-fixed bg-light ${span}`} aria-label="Contact us">
           <IoMailOutline />
         </span>
         <h1 className={h1}>Contact Us</h1>
